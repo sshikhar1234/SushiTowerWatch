39bc496d092ad679e6463935cbd882b629bb3fb8
sshikhar1234 <sshikharshah@gmail.com>
2019-10-30 10:59:29 -0400

    Initial Commit

---
+++ SushiTowerWatch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SushiTowerWatch
+//
+//  Created by Shikhar Shah on 2019-10-30.
+//  Copyright © 2019 Lambton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
